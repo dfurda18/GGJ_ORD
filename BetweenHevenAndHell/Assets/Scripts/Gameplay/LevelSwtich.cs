baf3bc0baf053911f322b7b03fb23358f880fc92
Darío Urdapilleta <dfurda18@gmail.com>
2022-01-30 14:10:03 -0400

    audio

---
+++ BetweenHevenAndHell/Assets/Scripts/Gameplay/LevelSwtich.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSwtich : MonoBehaviour
+{
+    public GameObject heavenLevel;
+    public GameObject hellLevel;
+    private bool heaven;
+    void Start()
+    {
+        heaven = false;
+        hellLevel.SetActive(true);
+        heavenLevel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void SwitchLevel()
+    {
+        if (heaven)
+        {
+            heaven = false;
+            hellLevel.SetActive(true);
+            heavenLevel.SetActive(false);
+        }
+        else
+        {
+            heaven = true;
+            heavenLevel.SetActive(true);
+            hellLevel.SetActive(false);
+        }
+    }
+}
